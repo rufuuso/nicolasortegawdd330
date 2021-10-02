55ad40c3d95ae1b3c23bc1c76b7d823064004955
rufuuso <rufuuso@gmail.com>
2021-10-02 11:05:40 -0300

    week 3

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const links = [
+    {
+      label: "Week 1",
+      url: "week1/index.html"
+    }
+  ]
+
+  
+
+for (let i = 0; i < links.length; i++ ) 
+    {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.textContent = links[i].label;
+        a.setAttribute('href', links[i].url);
+        li.appendChild(a);
+        document.querySelector('ol').appendChild(li);
+    }
